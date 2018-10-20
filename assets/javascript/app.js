bec5ed927f668145e15405d7354496f179d5699e
Media Man <matt.zuckermann@my.wheaton.edu>
2018-10-19 19:41:22 -0700

    Update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,7 @@
+//VARIABLES
+//=========
+var totalRight = 0
+var answers = ["Mild High Club", "Slowdive", "Kevin Abstract", "A ghost is birthed"]
 var intervalId;
 var clockRunning = false;
 var stopwatch = {
@@ -17,6 +21,7 @@ var stopwatch = {
             $("#displayTime").text(converted);
         }
     },
+
     timeConverter: function (t) {
         var minutes = Math.floor(t / 60);
         var seconds = t - (minutes * 60);
@@ -31,146 +36,73 @@ var stopwatch = {
         }
         return minutes + ":" + seconds;
     }
-};
+}
+//=========
+//FUNCTIONS
+//=========
 
-//Event listeners for starting the game
+//for loop for checking if answer was correct
+function checkAnswer() {
+    alert("You got it right!");
+    this.totalRight++;
+    $("#totalRight").text(totalRight);
+}
+
+function reset() {
+    stopwatch.time = 15;
+
+}
+
+//==============================
+//EVENT LISTENERS AND ACTIVATORS
+//==============================
+
+//Event Listeners for Starting and Restarting the Game
 $("#startButton").on("click", function () {
-    $("#displayTime, #display1").show()
+    $("#displayTime, #display1").show();
     $("#startButton").hide();
 });
+$("#restartButton").on("click", function () {
+    $("#displayTime, #display1").show();
+    $("#display5, #restartButton").hide();
+});
 
+//Starting & Restarting the Game
 $("#startButton").on("click", stopwatch.start);
+$("#restartButton").on("click", stopwatch.start);
 
 //Answers: Batch One
-$("'#answerChoice1a, #answerChoice2a, #answerChoice3a, #answerChoice4a").on("click", function() {
+$("#answerChoice-A1, #answerChoice-A2, #answerChoice-A3, #answerChoice-A4").on("click", function () {
+    if ($(this).attr("data-name") === "Mild High Club") {
+        checkAnswer();
+    }
     $("#display1").hide();
     $("#display2").show();
-    alert("This much is working!");
-    console.log(this);
 });
 
 //Answers: Batch Two
-$("#answerChoice1b, #answerChoice2b, #answerChoice3b, #answerChoice4b").on("click", function() {
+$("#answerChoice-B1, #answerChoice-B2, #answerChoice-B3, #answerChoice-B4").on("click", function () {
+    if ($(this).attr("data-name") === "Slowdive") {
+        checkAnswer();
+    }
     $("#display2").hide();
     $("#display3").show();
-    alert("This much is working!");
-    console.log(this);
 });
 
 //Answers: Batch Three
-$("#answerChoice1c, #answerChoice2c, #answerChoice3c, #answerChoice4c").on("click", function() {
+$("#answerChoice-C1, #answerChoice-C2, #answerChoice-C3, #answerChoice-C4").on("click", function () {
+    if ($(this).attr("data-name") === "Kevin Abstract") {
+        checkAnswer();
+    }
     $("#display3").hide();
     $("#display4").show();
-    alert("This much is working!");
-    console.log(this);
 });
 
 //Answers: Batch Four
-$("#answerChoice1d, #answerChoice2d, #answerChoice3d, #answerChoice4d").on("click", function() {
+$("#answerChoice-D1, #answerChoice-D2, #answerChoice-D3, #answerChoice-D4").on("click", function () {
+    if ($(this).attr("data-name") === "A ghost is birthed") {
+        checkAnswer();
+    }
     $("#display4, #displayTime").hide();
-    $("#display5").show();
-    alert("This much is working!");
-    console.log(this);
+    $("#display5, #restartButton").show();
 });
-
-
-
-
-////If statements to pair up with data-name within event listener to display if answer was right or wrong
-// $("#answerChoice1").on("click", function chooseAnswer1() {
-//     alert("You got it right!")
-// })
-// $("#answerChoice2").on("click", function chooseAnswer2() {
-//     alert("You got it wrong!")
-// })
-// $("#answerChoice3").on("click", function chooseAnswer3() {
-//     alert("You got it wrong!")
-// })
-// $("#answerChoice4").on("click", function chooseAnswer4() {
-//     alert("You got it wrong!")
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //Answers: Batch Two
-// $("#answerChoice5").on("click", function chooseAnswer1() {
-//     alert("You got it wrong!")
-//     $("#display2").hide();
-//     $("#display3").show();
-// })
-// $("#answerChoice6").on("click", function chooseAnswer2() {
-//     alert("You got it right!")
-//     $("#display2").hide();
-//     $("#display3").show();
-// })
-// $("#answerChoice7").on("click", function chooseAnswer3() {
-//     alert("You got it wrong!")
-//     $("#display2").hide();
-//     $("#display3").show();
-// })
-// $("#answerChoice8").on("click", function chooseAnswer4() {
-//     alert("You got it wrong!")
-//     $("#display2").hide();
-//     $("#display3").show();
-// })
-
-// //Answers: Batch Three
-// $("#answerChoice9").on("click", function chooseAnswer1() {
-//     alert("You got it wrong!")
-//     $("#display3").hide();
-//     $("#display4").show();
-// })
-// $("#answerChoice10").on("click", function chooseAnswer2() {
-//     alert("You got it wrong!")
-//     $("#display3").hide();
-//     $("#display4").show();
-// })
-// $("#answerChoice11").on("click", function chooseAnswer3() {
-//     alert("You got it right!")
-//     $("#display3").hide();
-//     $("#display4").show();
-// })
-// $("#answerChoice12").on("click", function chooseAnswer4() {
-//     alert("You got it wrong!")
-//     $("#display3").hide();
-//     $("#display4").show();
-// })
-
-// //Answers: Batch Four
-// $("#answerChoice13").on("click", function chooseAnswer1() {
-//     alert("You got it wrong!")
-//     $("#display4").hide();
-//     $("#display5").show();
-// })
-// $("#answerChoice14").on("click", function chooseAnswer2() {
-//     alert("You got it wrong!")
-//     $("#display4").hide();
-//     $("#display5").show();
-// })
-// $("#answerChoice15").on("click", function chooseAnswer3() {
-//     alert("You got it wrong!")
-//     $("#display4").hide();
-//     $("#display5").show();
-// })
-// $("#answerChoice16").on("click", function chooseAnswer4() {
-//     alert("You got it right!")
-//     $("#display4").hide();
-//     $("#display5").show();
-// })
